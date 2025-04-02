e4de5a2dbf3ad77568eec41ac5014a84ba46cf2c
hassan alwaili <hassan.alwaili@hotmail.com>
2025-04-02 20:27:08 +0300

    Fix employee filter field name and add schema extension for Step registration

--- config/apps/promos/filters.py
+++ config/apps/promos/filters.py
@@ -34,7 +34,7 @@ class PromotionFilter(django_filters.FilterSet):
     # promotion_order_no = django_filters.CharFilter(field_name='promotion_order_no', lookup_expr='icontains')  # ✅ Case-insensitive name search
     min_date = django_filters.DateFilter(field_name='promotion_date', lookup_expr='gte')
     max_date = django_filters.DateFilter(field_name='promotion_date', lookup_expr='lte')
-    employee = django_filters.CharFilter(field_name='employee__name', lookup_expr='icontains')
+    employee = django_filters.CharFilter(field_name='employee__employee_name', lookup_expr='icontains')
 
     class Meta:
         model = Promotion
--- config/apps/step_stage/views.py
+++ config/apps/step_stage/views.py
@@ -3,6 +3,13 @@ from .models import Step , Stage
 from .serializers import StepSerializer, StageSerializer
 from core.views import BaseModelViewSet
 from core.pagination import CustomPageNumberPagination
+from drf_spectacular.utils import extend_schema
+
+@extend_schema(
+    request=StepSerializer,
+    responses={201: StepSerializer},
+    description="Register a new user"
+)
 
 class StepViewSet(BaseModelViewSet):
     queryset = Step.objects.all().order_by('step_id')
